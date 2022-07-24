fd9de9c64dd3ec854bedade1247755869230c631
qingleizhai <qingleizhai@yeah.net>
2022-07-24 23:47:59 +0800

    add about h11

---
+++ h_11/melodic/mbot_navigation/scripts/exploring_maze_pro.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+ 
+import roslib;
+import rospy  
+import actionlib  
+import random
+from actionlib_msgs.msg import *  
+from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist  
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal  
+from std_msgs.msg import Int8
+
+STATUS_EXPLORING    = 0
+STATUS_CLOSE_TARGET = 1
+STATUS_GO_HOME      = 2
+
+class ExploringMaze():
+    def __init__(self):  
+        rospy.init_node('exploring_maze_pro', anonymous=True)  
+        rospy.on_shutdown(self.shutdown)  
+
+        # 在每个目标位置暂停的时间 (单位：s)
+        self.rest_time = rospy.get_param("~rest_time", 0.5)  
+
+        # 发布控制机器人的消息  
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=5) 
+ 
+        # 创建一个Subscriber，订阅/exploring_cmd
+        rospy.Subscriber("/exploring_cmd", Int8, self.cmdCallback)
+
+        # 订阅move_base服务器的消息  
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)  
+
+        rospy.loginfo("Waiting for move_base action server...")  
+        
+        # 60s等待时间限制  
+        self.move_base.wait_for_server(rospy.Duration(60))  
+        rospy.loginfo("Connected to move base server")  
+ 
+        # 保存运行时间的变量   
+        start_time = rospy.Time.now()  
+        running_time = 0  
+
+        rospy.loginfo("Starting exploring maze")  
+        
+        # 初始位置
+        start_location = Pose(Point(0, 0, 0), Quaternion(0.000, 0.000, 0.709016873598, 0.705191515089))  
+ 
+        locations = []  
+
+        locations.append(Pose(Point(-0.002, 7.663, 0.000),  Quaternion(0.000, 0.000, 0.709016873598, 0.705191515089)))  
+######################################请补充更多位置（开始）######################################################
+
+
+
+
+######################################请补充更多位置（结束）######################################################
+
+        # 命令初值
+        self.exploring_cmd = 0
+        
+        location = 0
+       
+        # 开始主循环，随机导航  
+        while not rospy.is_shutdown():
+######################################请补充主循环中的代码（开始）######################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################请补充主循环中的代码（结束）######################################################
+        self.shutdown()
+
+    def cmdCallback(self, msg):
+        rospy.loginfo("Receive exploring cmd : %d", msg.data)
+        self.exploring_cmd = msg.data
+
+        if self.exploring_cmd is STATUS_CLOSE_TARGET:
+            rospy.loginfo("Stopping the robot...")  
+            self.move_base.cancel_goal() 
+
+    def shutdown(self):  
+        rospy.loginfo("Stopping the robot...")  
+        self.move_base.cancel_goal()  
+        rospy.sleep(2)  
+        self.cmd_vel_pub.publish(Twist())
+        rospy.sleep(1)  
+
+def trunc(f, n):  
+    slen = len('%.*f' % (n, f))  
+    return float(str(f)[:slen])  
+
+if __name__ == '__main__':  
+    try:  
+        ExploringMaze()  
+        rospy.spin()  
+
+    except rospy.ROSInterruptException:  
+        rospy.loginfo("Exploring maze finished.")
